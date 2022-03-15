b55ef4d9bc28381f647e9a9462b1966cff3447ec
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2022-03-16 01:44:24 +0530

    added relation concept

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -14,4 +14,9 @@ concept TransformationRef = requires(Domain const &d, Func func) {
 template<typename Func, typename Domain>
 concept Transformation =
   TransformationValue<Func, Domain> or TransformationRef<Func, Domain>;
+
+template<typename Func, typename Domain>
+concept Relation = requires(Domain const &d, Func func) {
+  { func(d, d) } -> std::same_as<bool>;
+};
 }// namespace eop
--- src/ex_4_4.cpp
+++ src/ex_4_4.cpp
@@ -1,4 +1,6 @@
-template<typename Domain, typename Comparator>
+#include "concepts.hpp"
+
+template<typename Domain, eop::Relation<Domain> Comparator>
 Domain const &select_2_4_ab_cd(Domain const &a,
   Domain const &b,
   Domain const &c,
@@ -11,7 +13,7 @@ Domain const &select_2_4_ab_cd(Domain const &a,
   }
 }
 
-template<typename Domain, typename Comparator>
+template<typename Domain, eop::Relation<Domain> Comparator>
 Domain const &select_2_4_ab(Domain const &a,
   Domain const &b,
   Domain const &c,
@@ -24,7 +26,7 @@ Domain const &select_2_4_ab(Domain const &a,
   }
 }
 
-template<typename Domain, typename Comparator>
+template<typename Domain, eop::Relation<Domain> Comparator>
 Domain const &select_2_4(Domain const &a,
   Domain const &b,
   Domain const &c,
