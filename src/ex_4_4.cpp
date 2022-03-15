2a2801f5a33dd92549466c7f4e00464790e6075a
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2022-03-16 01:38:15 +0530

    exercise 4.4

---
+++ src/ex_4_4.cpp
@@ -0,0 +1,38 @@
+template<typename Domain, typename Comparator>
+Domain const &select_2_4_ab_cd(Domain const &a,
+  Domain const &b,
+  Domain const &c,
+  Domain const &d,
+  Comparator cmp) {
+  if (cmp(d, b)) {
+    return select_1_2(a, d);
+  } else {
+    return select_1_2(b, c);
+  }
+}
+
+template<typename Domain, typename Comparator>
+Domain const &select_2_4_ab(Domain const &a,
+  Domain const &b,
+  Domain const &c,
+  Domain const &d,
+  Comparator cmp) {
+  if (cmp(d, c)) {
+    return select_2_4_ab_cd(a, b, d, c);
+  } else {
+    return select_2_4_ab_cd(a, b, c, d);
+  }
+}
+
+template<typename Domain, typename Comparator>
+Domain const &select_2_4(Domain const &a,
+  Domain const &b,
+  Domain const &c,
+  Domain const &d,
+  Comparator cmp) {
+  if (cmp(b, a)) {
+    return select_2_4_ab(b, a, c, d);
+  } else {
+    return select_2_4_ab(a, b, c, d);
+  }
+}
